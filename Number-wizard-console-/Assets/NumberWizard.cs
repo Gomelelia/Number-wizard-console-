65af190cc7ff7800f4a8fee58647cdb97a584883
Gomelelia <eliaxdoo96@gmail.com>
2021-04-28 07:24:48 +0300

    Update NumberWizard.cs

--- Number-wizard-console-/Assets/NumberWizard.cs
+++ Number-wizard-console-/Assets/NumberWizard.cs
@@ -7,10 +7,16 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
+
         Debug.Log("Welcome to number wizard");
-        Debug.Log("Pick an number");
-        Debug.Log("The highest number you can pick is 1000");
-        Debug.Log("The lowest number you can pick is 1");
+        Debug.Log("Pick a number");
+        Debug.Log("The highest number you can pick is : " + max);
+        Debug.Log("The lowest number you can pick is : " + min);
+        Debug.Log("Tell me if your number is higher or lower then 500");
+        Debug.Log("Push up = higher, Push down = Lower, Push Enter = Correct");
 
 
     }
@@ -18,6 +24,19 @@ public class NumberWizard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        { 
+            Debug.Log("Up Arrow key was pressed.");
+        }
+      
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        { 
+            Debug.Log("Down Arrow key was pressed.");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Return key was pressed.");
+        }
     }
 }

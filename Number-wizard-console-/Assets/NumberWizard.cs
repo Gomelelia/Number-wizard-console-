f6af9f6716d03eaa620543ca4fbd3bbb52888f8a
Gomelelia <eliaxdoo96@gmail.com>
2021-04-28 06:45:44 +0300

    Number wizard console

---
+++ Number-wizard-console-/Assets/NumberWizard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to number wizard");
+        Debug.Log("Pick an number");
+        Debug.Log("The highest number you can pick is 1000");
+        Debug.Log("The lowest number you can pick is 1");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

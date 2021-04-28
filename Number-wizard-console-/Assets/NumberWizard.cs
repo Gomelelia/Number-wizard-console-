4bbedb90207d81c974f75216246bcdc0041ba198
Gomelelia <eliaxdoo96@gmail.com>
2021-04-28 21:00:22 +0300

    full game loop Number Wizard Console

--- Number-wizard-console-/Assets/NumberWizard.cs
+++ Number-wizard-console-/Assets/NumberWizard.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class NumberWizard : MonoBehaviour
 {
 
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
 
+        StartGame();
+
+    }
+
+    void StartGame()
+    {
+
+        max = 1000;
+        min = 1;
+        guess = 500;
 
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number");
@@ -23,33 +32,35 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Tell me if your number is higher or lower then: " + guess);
         Debug.Log("Push up = higher, Push down = Lower, Push Enter = Correct");
         max = max + 1;
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
-        { 
+        {
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("is it higher or lower then..." + guess);
-
-          
-
+            NextGuess();
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
-        { 
+        {
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("is it higher or lower then..." + guess);
+            NextGuess();
         }
 
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("correct");
+            StartGame();
         }
+    }
+
+    void NextGuess()
+    {
+
+        guess = (max + min) / 2;
+        Debug.Log("is it higher or lower then..." + guess);
+
     }
 }

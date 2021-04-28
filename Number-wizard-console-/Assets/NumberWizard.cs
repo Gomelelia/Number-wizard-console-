d49a48e7817e2dc4b9a7ee62c94908a623b6116d
Gomelelia <eliaxdoo96@gmail.com>
2021-04-28 10:41:08 +0300

    Update NumberWizard.cs

--- Number-wizard-console-/Assets/NumberWizard.cs
+++ Number-wizard-console-/Assets/NumberWizard.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
+        
 
 
         Debug.Log("Welcome to number wizard");
@@ -27,11 +32,18 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         { 
             Debug.Log("Up Arrow key was pressed.");
+            min = guess;
+            guess = (max + min) / 2;
+            Debug.Log(guess);
+
         }
-      
+
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         { 
             Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            guess = (max + min) / 2;
+            Debug.Log(guess);
         }
 
         else if (Input.GetKeyDown(KeyCode.Return))
